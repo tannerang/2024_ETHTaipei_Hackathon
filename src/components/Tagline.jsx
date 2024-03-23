ea221236ba42a33d1d2fcc19153da4698f6ac6cf
Tanner Ang <ang713943@gmail.com>
2024-03-24 04:40:49 +0800

    update TagLine font size

--- src/components/Tagline.jsx
+++ src/components/Tagline.jsx
@@ -4,7 +4,7 @@ const TagLine = ({ className, children }) => {
   return (
     <div className={`tagline flex items-center ${className || ""}`}>
       {brackets("left")}
-      <div className="font-bold text-2xl mx-3 text-n-3">{children}</div>
+      <div className="text-lg mx-3 text-n-3">{children}</div>
       {brackets("right")}
     </div>
   );
