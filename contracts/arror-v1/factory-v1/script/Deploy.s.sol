7ffe5411b610bcbb7f7dc74dfd3b5bb2950681dd
Tanner Ang <ang713943@gmail.com>
2024-03-24 01:03:11 +0800

    add factory-v1/script/Deploy.s.sol

--- contracts/arror-v1/factory-v1/script/Deploy.s.sol
+++ contracts/arror-v1/factory-v1/script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.21;
 
 import {Script, console2} from "forge-std/Script.sol";
 import "../src/ArrorFactory.sol";
@@ -7,15 +7,21 @@ import "../src/ArrorFactory.sol";
 contract DeployScript is Script {
     
     ArrorFactory public immutable factory;
+    address public dysonRouter;
+    address public dysonFactory;
 
-    function setUp() public {}
+
+    function setUp() public {
+        dysonRouter = vm.envAddress("DYSON_ROUTER");
+        dysonFactory = vm.envAddress("DYSON_FACTORY");
+    }
 
     function run() public {
         vm.startBroadcast(vm.envUint("DEV_PRIVATE_KEY"));
 
-        // factory = new ArrorFactory(address(dysonRouter), address(dysonFactory));
+        factory = new ArrorFactory(dysonRouter, dysonFactory);
 
         vm.stopBroadcast();
-        // console2.log("factory = ", address(factory));
+        console2.log("factory = ", address(factory));
     }
 }
