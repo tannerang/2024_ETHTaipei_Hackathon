0ac58fd8d5e63efe08754428b9ee87ce1591690c
Tanner Ang <ang713943@gmail.com>
2024-04-03 23:57:42 +0800

    add testPairAddLiquidityETH()

--- contracts/arror-v1/factory-v1/src/ArrorUniFactory.sol
+++ contracts/arror-v1/factory-v1/src/ArrorUniFactory.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "./ArrorERC404.sol";
-import { IUniswapV2Router01 } from "v2-periphery/interfaces/IUniswapV2Router01.sol";
+import { IUniswapV2Router02 } from "v2-periphery/interfaces/IUniswapV2Router02.sol";
 import { IUniswapV2Factory } from "v2-core/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Pair } from "v2-core/interfaces/IUniswapV2Pair.sol";
 //import { TestERC20 } from "../contracts/test/TestERC20.sol";
@@ -13,16 +13,17 @@ contract ArrorUniFactory {
     // IUniswapV2Factory public constant UNISWAP_V2_FACTORY =
     // IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     // address public constant WETH9 = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IUniswapV2Router01 public UNISWAP_V2_ROUTER; 
+    IUniswapV2Router02 public UNISWAP_V2_ROUTER; 
     IUniswapV2Factory public UNISWAP_V2_FACTORY;
 
 
     event ArrorERC404Created(address indexed newArrorERC404Address, address indexed creator);
     event ArrorERC404CreatedPair(address indexed tokenA, address indexed tokenB, address indexed pair);
+    event ArrorERC404AddLiquidityETH(address indexed newArrorERC404Address, address indexed factory, uint indexed amount);
 
 
     constructor(address _uniswapV2Router, address _uniswapV2Factory) {
-        UNISWAP_V2_ROUTER = IUniswapV2Router01(_uniswapV2Router);
+        UNISWAP_V2_ROUTER = IUniswapV2Router02(_uniswapV2Router);
         UNISWAP_V2_FACTORY = IUniswapV2Factory(_uniswapV2Factory);
     }
 
@@ -37,7 +38,7 @@ contract ArrorUniFactory {
         string memory tokenURI4_,
         string memory tokenURI5_,
         address tokenB_
-    ) public returns (ArrorERC404 newArror, address uniPair) {
+    ) public payable returns (ArrorERC404 newArror, address uniswapV2Pair) {
         newArror = new ArrorERC404(
             name_,
             symbol_,
@@ -53,16 +54,33 @@ contract ArrorUniFactory {
             tokenURI4_,
             tokenURI5_
         );
-        uniPair = _createPair(address(newArror), tokenB_);
+        uniswapV2Pair = _createPair(address(newArror), tokenB_);
         // Set the Dyson Finance as exempt.
         newArror.setERC721TransferExempt(address(UNISWAP_V2_FACTORY), true);
         newArror.setERC721TransferExempt(address(UNISWAP_V2_ROUTER), true);
+        newArror.setERC721TransferExempt(uniswapV2Pair, true);
         emit ArrorERC404Created(address(newArror), msg.sender);
+
+        // Add liquidity
+        _addLiquidityETH(newArror);
     }
 
-    function _createPair(address tokenA, address tokenB) internal returns (address uniPair){
-        uniPair = UNISWAP_V2_FACTORY.createPair(tokenA, tokenB);
-        require(uniPair != address(0), "Factory: UNIPAIR_EXISTS");
-        emit ArrorERC404CreatedPair(tokenA, tokenB, uniPair);
+    function _createPair(address tokenA, address tokenB) internal returns (address uniswapV2Pair){
+        uniswapV2Pair = UNISWAP_V2_FACTORY.createPair(tokenA, tokenB);
+        require(uniswapV2Pair != address(0), "Factory: UNISWAPV2PAIR_EXISTS");
+        emit ArrorERC404CreatedPair(tokenA, tokenB, uniswapV2Pair);
+    }
+
+    function _addLiquidityETH(ArrorERC404 newArror) internal {
+        newArror.erc20Approve(address(UNISWAP_V2_ROUTER), newArror.erc20TotalSupply());
+        UNISWAP_V2_ROUTER.addLiquidityETH{value:msg.value}(
+            address(newArror),
+            1 * 10 ** newArror.decimals(),
+            0,
+            0,
+            msg.sender,
+            (block.timestamp+999999999)
+        );
+        emit ArrorERC404AddLiquidityETH(address(newArror), address(this), 1 * 10 ** newArror.decimals());
     }
 }
--- contracts/arror-v1/factory-v1/test/ArrorUniFactory.t.sol
+++ contracts/arror-v1/factory-v1/test/ArrorUniFactory.t.sol
@@ -6,6 +6,10 @@ import {ArrorUniFactory} from "../src/ArrorUniFactory.sol";
 import {ArrorERC404} from "../src/ArrorERC404.sol";
 import {IUniswapV2Factory} from "contracts/interfaces/IUniswapV2Factory.sol";
 import {IUniswapV2Router01} from "contracts/interfaces/IUniswapV2Router01.sol";
+import {IUniswapV2Router02} from "contracts/interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Pair} from "contracts/interfaces/IUniswapV2Pair.sol";
+import {IWETH} from "contracts/interfaces/IWETH.sol";
+import {IERC20} from "contracts/interfaces/IERC20.sol";
 
 
 contract ArrorUniFactoryTest is Test {
@@ -14,15 +18,62 @@ contract ArrorUniFactoryTest is Test {
     IUniswapV2Factory public UNISWAP_V2_FACTORY_MAINNET = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     address public constant WETH9 = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
+    IUniswapV2Router02 public UNISWAP_V2_ROUTER_SEPOLIA = IUniswapV2Router02(0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008);
+    IUniswapV2Factory public UNISWAP_V2_FACTORY_SEPOLIA = IUniswapV2Factory(0x7E0987E5b3a30e3f2828572Bb659A548460a3003);
+    IWETH public WETH9_SEPOLIA = IWETH(0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9);
+
+    ArrorERC404 arrorERC404;
+    address uniswapV2Pair;
+
+    address public user = makeAddr("User");
+
+    
     function setUp() public {
-        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
-        arrorUniFactory = new ArrorUniFactory(address(UNISWAP_V2_ROUTER_MAINNET), address(UNISWAP_V2_FACTORY_MAINNET));
+        // vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+        // arrorUniFactory = new ArrorUniFactory(address(UNISWAP_V2_ROUTER_MAINNET), address(UNISWAP_V2_FACTORY_MAINNET));
+
+        vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
+        arrorUniFactory = new ArrorUniFactory(address(UNISWAP_V2_ROUTER_SEPOLIA), address(UNISWAP_V2_FACTORY_SEPOLIA));
+
+        (arrorERC404, uniswapV2Pair) = arrorUniFactory.createERC404("name", "symbol", 18, 100, "uri1", "uri2", "uri3", "uri4", "uri5", address(WETH9_SEPOLIA));
+        console2.log("arrorERC404 address:", address(arrorERC404));
+        console2.log("uniswapV2Pair address:", uniswapV2Pair);
+
+        deal(user, 1000 ether);
+        deal(address(arrorERC404), user, 10000 * 10 ** arrorERC404.decimals());
     }
 
+    /*
     function testCreateERC404() public {
-        (ArrorERC404 arrorERC404, address uniPair) = arrorUniFactory.createERC404("name", "symbol", 18, 100, "uri1", "uri2", "uri3", "uri4", "uri5", WETH9);
+        // (ArrorERC404 arrorERC404, address uniswapV2Pair) = arrorUniFactory.createERC404("name", "symbol", 18, 100, "uri1", "uri2", "uri3", "uri4", "uri5", WETH9);
+
+        (arrorERC404, uniswapV2Pair) = arrorUniFactory.createERC404("name", "symbol", 18, 100, "uri1", "uri2", "uri3", "uri4", "uri5", WETH9_SEPOLIA);
         console2.log("arrorERC404 address:", address(arrorERC404));
-        console2.log("uniPair address:", uniPair);
+        console2.log("uniswapV2Pair address:", uniswapV2Pair);
+    }
+    */
+
+    function testPairAddLiquidityETH() public {
+
+        vm.startPrank(user);
+        arrorERC404.erc20Approve(address(UNISWAP_V2_ROUTER_SEPOLIA), 10000 * 10 ** arrorERC404.decimals());
+        UNISWAP_V2_ROUTER_SEPOLIA.addLiquidityETH{value:10 ether}(
+            address(arrorERC404),
+            10 * 10 ** arrorERC404.decimals(),
+            0,
+            0,
+            user,
+            (block.timestamp+9999)
+        );
+        vm.stopPrank();
+
+        IUniswapV2Pair wethArrorPair = IUniswapV2Pair(UNISWAP_V2_FACTORY_SEPOLIA.getPair(address(WETH9_SEPOLIA), address(arrorERC404)));
+        (uint112 _reserve0, uint112 _reserve1,) = wethArrorPair.getReserves();
+        console2.log(_reserve0);
+        console2.log(_reserve1);
+
+        // assertEq(_reserve0, 0);
+        // assertEq(_reserve1, 0);
     }
 
 }
